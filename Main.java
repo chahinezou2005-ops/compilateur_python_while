da11372691b2100b69de040874c8b33ab64fa53e
chahinezou2005-ops <chahinez.ou2005@gmail.com>
2025-11-30 18:18:05 +0100

    analyseur lexical terminée

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+       
+
+
+             String code = """
+            chahinez = 20
+            age = 18.5
+            while age > 0:
+                print("Bonjour Chahinez")
+                age = age - 1
+            else:
+                print("Terminé
+            # commentaire ignoré
+            """;
+
+
+        
+        Lexer lexer = new Lexer(code);
+        lexer.analyser();
+        lexer.afficher();
+    }
+}
